72a504ea3cd38b17627d690442681c2bcf6422a9
solchan87 <perudot@gmail.com>
2018-04-02 16:09:14 +0900

    ADD Color struct

--- OverEats/Source/Model/AppColor.swift
+++ OverEats/Source/Model/AppColor.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 struct AppColor {
-    let main = UIColor(red: 71, green: 161, blue: 44, alpha: 1)
-    let Base = UIColor(red: 242, green: 242, blue: 244, alpha: 1)
-    let lightGray = UIColor(red: 156, green: 156, blue: 158, alpha: 1)
+    // logo에 사용되는 기본적인 연두색
+    let mainGreen = UIColor(red: 71, green: 161, blue: 44, alpha: 1)
+    // view background에 사용되는 색
+    let Base = UIColor(red: 242, green: 242, blue: 242, alpha: 1)
+    // font에 사용되는 밝은 회색
+    let lightGray = UIColor(red: 155, green: 155, blue: 155, alpha: 1)
+    // font에 사용되는 기본 회색
+    let defaultGray = UIColor(red: 110, green: 110, blue: 110, alpha: 1)
+    // font에 사용되는 어두운 회색
+    let darkGray = UIColor(red: 55, green: 55, blue: 55, alpha: 1)
+    // 기본 select 검은색
+    let black = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
 }
