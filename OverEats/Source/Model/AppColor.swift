d3cfe7c9ec6a453d9458d4b072a224b9a44f1790
solchan87 <perudot@gmail.com>
2018-04-02 15:22:45 +0900

    ADD AppColor

--- OverEats/Source/Model/AppColor.swift
+++ OverEats/Source/Model/AppColor.swift
@@ -6,8 +6,10 @@
 //  Copyright © 2018년 solchan ahn. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct AppColor {
-    
+    let main = UIColor(red: 71, green: 161, blue: 44, alpha: 1)
+    let Base = UIColor(red: 242, green: 242, blue: 244, alpha: 1)
+    let lightGray = UIColor(red: 156, green: 156, blue: 158, alpha: 1)
 }
