15c1655062ec274286f4cab4783c7fd42f20dda7
SangWookPark <wkahdla12346@gmail.com>
2018-04-02 14:32:52 +0900

    App Color Modified Update

---
+++ OverEats/Source/Model/AppColor.swift
@@ -0,0 +1,13 @@
+//
+//  AppColor.swift
+//  OverEats
+//
+//  Created by 안솔찬 on 2018. 4. 2..
+//  Copyright © 2018년 solchan ahn. All rights reserved.
+//
+
+import Foundation
+
+struct AppColor {
+    
+}
