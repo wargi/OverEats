1b6ac873c58b076d8fd57cd31c1f83a25d2f5a18
solchan87 <perudot@gmail.com>
2018-04-06 14:56:19 +0900

    modify network data

--- OverEats/Source/Model/MainModel.swift
+++ OverEats/Source/Model/MainModel.swift
@@ -8,19 +8,59 @@
 
 import Foundation
 
+struct User {
+    let user: [User] // 유저 데이터
+    let token: String! // 토큰 값
+}
+
+struct UserDetail {
+    let id: String!
+    let userName: String! // 유저 아이디
+    let firstName: String! // 이름
+    let lastName: String! // 성
+    let phoneNumber: String! // 폰 번호
+    let imageURL: String? // 이미지 URL
+}
+
+struct CreditCard {
+    let creditCard: String? // 결제할 카드
+}
+
+struct Favorites {
+    let favoriteRestaurant: [String] //즐겨 찾는 매장의 고유번호를 저장
+}
+
 struct Notice {
-    var id: Int!
-    var title: String?
-    var subTitle: String?
-    var imageURL: String!
+    let id: String! // 공지 고유 번호
+    let title: String? // 대제목
+    let subTitle: String? // 소제목
+    let imageURL: String! // 이미지 URL
 }
 
 struct Restaurant {
-    var id: Int!
-    var name: String!
-    var tag: [String]
-    var score: Float!
-    var time: Int!
-    var account: String?
-    var imageURL: String!
+    let id: String! // 매장 고유 번호
+    let name: String! // 매장 명
+    let isOpen: Bool! // 매장 오픈 여부
+    let address: String! // 매장 주소
+    let description: String? // 매장 설명
+    let score: Float? // 매장 점수
+    let imageURL: String! // 매장 표시 이미지
+    let minDeliveryTime: Int! // 매장 최소 조리시간
+    let maxDeliveryTime: Int! // 매장 최대 조리시간
+    let menuList: [Menu]? // 메뉴 목록
+    let category: [Category]? // 식당의 유형 예)카페, 한식
+}
+
+struct Menu {
+    let id: String! // 음식 고유 번호
+    let price: Int! // 음식 가격
+    let foodName: String! // 음식 이름
+    let foodImage: String? // 음식 사진
+    let foodDescription: String? // 음식 설명
+}
+
+struct Category {
+    let id: String! // 카테고리 고유 번호
+    let name: String! // 카테고리명
+    let imageURL: String? // 카테고리 이미지
 }
--- OverEats/Source/Service/MainNetwork.swift
+++ OverEats/Source/Service/MainNetwork.swift
@@ -14,19 +14,19 @@ struct MainGet {
         var notices = [Notice]()
         
         // 1번 데이터
-        let notice1 = Notice(id: 1, title: "함께하면 더욱 맛있다!", subTitle: "친구 초대하고 7,000원 할인받으세요.", imageURL: "https://picsum.photos/800/500?random")
+        let notice1 = Notice(id: "1", title: "함께하면 더욱 맛있다!", subTitle: "친구 초대하고 7,000원 할인받으세요.", imageURL: "https://picsum.photos/800/500?random")
         notices.append(notice1)
         
         // 2번 데이터
-        let notice2 = Notice(id: 2, title: "", subTitle: "드디어! 오버이츠가 마포, 서대문 맛집을 배달합니다!", imageURL: "https://picsum.photos/800/500?random")
+        let notice2 = Notice(id: "2", title: "", subTitle: "드디어! 오버이츠가 마포, 서대문 맛집을 배달합니다!", imageURL: "https://picsum.photos/800/500?random")
         notices.append(notice2)
         
         // 3번 데이터
-        let notice3 = Notice(id: 3, title: "똑똑한 오버이츠 이용 꿀팁", subTitle: "탭하고 프로모션 코드, 상세주소 입력방법을 확인하세요.", imageURL: "https://picsum.photos/800/500?random")
+        let notice3 = Notice(id: "3", title: "똑똑한 오버이츠 이용 꿀팁", subTitle: "탭하고 프로모션 코드, 상세주소 입력방법을 확인하세요.", imageURL: "https://picsum.photos/800/500?random")
         notices.append(notice3)
         
         // 4번 데이터
-        let notice4 = Notice(id: 4, title: "", subTitle: "오버이츠만의  기간한정 더블 혜택, 지금 주문하세요!", imageURL: "https://picsum.photos/800/500?random")
+        let notice4 = Notice(id: "4", title: "", subTitle: "오버이츠만의  기간한정 더블 혜택, 지금 주문하세요!", imageURL: "https://picsum.photos/800/500?random")
         notices.append(notice4)
         
         completionHandler(notices)
@@ -37,28 +37,51 @@ struct MainGet {
         var restaurants = [Restaurant]()
         
         
-        let tag1 = ["일식"]
-        let restaurant1 = Restaurant(id: 1, name: "이치라쿠", tag: tag1, score: 4.2, time: 20,account: "테이스티로드에 소개된 라멘 맛집, 가로수길 피에스타 선정 맛집", imageURL: "https://picsum.photos/800/500?random")
+        let category1:[Category] = [
+            Category(id: "1", name: "일식", imageURL: "https://picsum.photos/500/500?random")
+        ]
+        let restaurant1 = Restaurant(id: "1", name: "이치라쿠", isOpen: true, address: "서초구 서초동 101-1", description: "테이스티로드에 소개된 라멘 맛집, 가로수길 피에스타 선정 맛집", score: 4.2, imageURL: "https://picsum.photos/800/500?random", minDeliveryTime: 10, maxDeliveryTime: 20, menuList: nil, category: category1)
         restaurants.append(restaurant1)
         
-        let tag2 = ["셀러드", "웰빙", "비건"]
-        let restaurant2 = Restaurant(id: 2, name: "알로하 포케 학동점", tag: tag2, score: 0, time: 20,account: "", imageURL: "https://picsum.photos/800/500?random")
+        
+        let category2:[Category] = [
+            Category(id: "2", name: "셀러드", imageURL: "https://picsum.photos/500/500?random"),
+            Category(id: "3", name: "웰빙", imageURL: "https://picsum.photos/500/500?random"),
+            Category(id: "4", name: "비건", imageURL: "https://picsum.photos/500/500?random")
+        ]
+        let restaurant2 = Restaurant(id: "2", name: "알로하 포케 학동점", isOpen: true, address: "서초구 서초동 101-1", description: "", score: 0, imageURL: "https://picsum.photos/800/500?random", minDeliveryTime: 10, maxDeliveryTime: 20, menuList: nil, category: category2)
         restaurants.append(restaurant2)
         
-        let tag3 = ["베이커리", "디저트", "커피", "차"]
-        let restaurant3 = Restaurant(id: 3, name: "컨버세이션", tag: tag3, score: 4.0, time: 20,account: "그림책에만 있는줄 알았던 엄청난 비주얼의 케이크", imageURL: "https://picsum.photos/800/500?random")
+        
+        let category3:[Category] = [
+            Category(id: "5", name: "베이커리", imageURL: "https://picsum.photos/500/500?random"),
+            Category(id: "6", name: "디저트", imageURL: "https://picsum.photos/500/500?random"),
+            Category(id: "7", name: "커피", imageURL: "https://picsum.photos/500/500?random"),
+            Category(id: "8", name: "차", imageURL: "https://picsum.photos/500/500?random")
+        ]
+        let restaurant3 = Restaurant(id: "3", name: "컨버세이션", isOpen: true, address: "서초구 서초동 101-1", description: "그림책에만 있는줄 알았던 엄청난 비주얼의 케이크", score: 4.0, imageURL: "https://picsum.photos/800/500?random", minDeliveryTime: 10, maxDeliveryTime: 20, menuList: nil, category: category3)
         restaurants.append(restaurant3)
         
-        let tag4 = ["일식"]
-        let restaurant4 = Restaurant(id: 4, name: "김태완 스시 논현점", tag: tag4, score: 0, time: 35,account: "이름을 걸고 만드는 신선한 사시미와 스시 전문점", imageURL: "https://picsum.photos/800/500?random")
+        
+        let category4:[Category] = [
+            Category(id: "1", name: "일식", imageURL: "https://picsum.photos/500/500?random")
+        ]
+        let restaurant4 = Restaurant(id: "4", name: "김태완 스시 논현점", isOpen: false, address: "서초구 서초동 101-1", description: "이름을 걸고 만드는 신선한 사시미와 스시 전문점", score: 0, imageURL: "https://picsum.photos/800/500?random", minDeliveryTime: 30, maxDeliveryTime: 40, menuList: nil, category: category4)
         restaurants.append(restaurant4)
         
-        let tag5 = ["일식"]
-        let restaurant5 = Restaurant(id: 5, name: "테루", tag: tag5, score: 0, time: 30,account: "", imageURL: "https://picsum.photos/800/500?random")
+        
+        let category5:[Category] = [
+            Category(id: "1", name: "일식", imageURL: "https://picsum.photos/500/500?random")
+        ]
+        let restaurant5 = Restaurant(id: "5", name: "테루", isOpen: true, address: "서초구 서초동 101-1", description: "", score: 0, imageURL: "https://picsum.photos/800/500?random", minDeliveryTime: 30, maxDeliveryTime: 40, menuList: nil, category: category5)
         restaurants.append(restaurant5)
         
-        let tag6 = ["양식", "이탈리안"]
-        let restaurant6 = Restaurant(id: 6, name: "셰프런", tag: tag6, score: 4.5, time: 20,account: "Top 스타셰프의 요리를 집에서 즐기세요!", imageURL: "https://picsum.photos/800/500?random")
+        
+        let category6:[Category] = [
+            Category(id: "9", name: "양식", imageURL: "https://picsum.photos/500/500?random"),
+            Category(id: "10", name: "이탈리안", imageURL: "https://picsum.photos/500/500?random")
+        ]
+        let restaurant6 = Restaurant(id: "6", name: "셰프런", isOpen: false, address: "서초구 서초동 101-1", description: "Top 스타셰프의 요리를 집에서 즐기세요!", score: 4.0, imageURL: "https://picsum.photos/800/500?random", minDeliveryTime: 10, maxDeliveryTime: 20, menuList: nil, category: category6)
         restaurants.append(restaurant6)
         
         completionHandler(restaurants)
