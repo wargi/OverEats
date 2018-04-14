4a17608a963ff9eb5ec6de81ec7040dbe8b54365
btw0118 <btw0118@gmail.com>
2018-04-14 12:07:16 +0900

    set storyboard BackButton

--- OverEats/Source/ViewController/Sign/SignViewController.swift
+++ OverEats/Source/ViewController/Sign/SignViewController.swift
@@ -14,20 +14,25 @@ class SignViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
+        //라운드 값 주기
         roundView.layer.cornerRadius = roundView.frame.size.height/6
  
     }
     
+    //로그인 버튼
     @IBAction func signInButton(_ sender: UIButton) {
       
+        //로그인 페이지로 넘어가기
         let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
         present(nextViewController, animated: true, completion: nil)
         
     }
     
+    //회원가입 버튼
     @IBAction func signUpButton(_ sender: UIButton) {
         
+        //회원가입 첫 페이지 약관부분으로 넘어가기
         let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "ToSViewController") as! ToSViewController
         present(nextViewController, animated: true, completion: nil)
         
